26babc8e240bc92b60591aa8f18fc165d6ee2751
henrythebuilder <henrythebuilder@yahoo.it>
2019-09-24 16:20:00 +0200

    Filter Pinout DocTest only for 'stub' platform

--- test/pinout_test.exs
+++ test/pinout_test.exs
@@ -1,11 +1,8 @@
 defmodule PinoutTest do
   use ExUnit.Case
 
-  # setup do
-  #   System.put_env("BREADBOARD_PLATFORM", "stub")
-  #   System.put_env("BREADBOARD_GPIO_INFO_NAME", "stub")
-  # end
-
-  doctest Breadboard.Pinout
+  if(Breadboard.GPIO.Utils.get_platform()==:stub) do
+    doctest Breadboard.Pinout
+  end
 
 end
