5050b97d88c139755fd40c6c821e5faeecd6c751
henrythebuilder <henrythebuilder@yahoo.it>
2019-10-01 14:17:53 +0200

    Refactoring

--- lib/breadboard/switch.ex
+++ lib/breadboard/switch.ex
@@ -13,7 +13,7 @@ defmodule Breadboard.Switch do
       iex> Breadboard.Switch.get_value(switch)
       1
 
-  ### switch with initial value
+  ### Switch with initial value
 
       iex> Breadboard.set_platform(:stub)
       iex> {:ok, switch1} = Breadboard.Switch.connect([pin: :gpio18, direction: :output, initial_value: 1])
@@ -23,7 +23,7 @@ defmodule Breadboard.Switch do
       iex> Breadboard.Switch.get_value(switch1)
       1
 
-  ### switch as input and output (Stub HAL with pair of GPIOs is connected)
+  ### Switch as input and output (Stub HAL with pair of GPIOs is connected)
       iex> if(Circuits.GPIO.info.name==:stub) do # only for Unit Test purpose
       ...> Breadboard.set_platform(:stub)
       ...> {:ok, switch0} = Breadboard.Switch.connect([pin: :gpio0, direction: :output, initial_value: 0])
@@ -35,7 +35,7 @@ defmodule Breadboard.Switch do
       ...> end
       1
 
-  ### Simple Interrupt test: open two pin as in and out plus a pin as test raised to one on irq notification
+  ### Simple Interrupt test:
       iex> if(Circuits.GPIO.info.name==:stub) do # only for Unit Test purpose
       ...>   Breadboard.set_platform(:stub)
       ...>   defmodule InterruptsTest do
@@ -46,8 +46,10 @@ defmodule Breadboard.Switch do
       ...>       Breadboard.Switch.turn_on(switch_test)
       ...>     end
       ...>   end
+      ...>   # open two pin as in and out ...
       ...>   {:ok, switch_in} = Breadboard.Switch.connect([pin: :gpio1, direction: :input])
       ...>   {:ok, switch_out} = Breadboard.Switch.connect([pin: :gpio1, direction: :output])
+      ...>   # ... plus a pin as test raised to one on irq notification
       ...>   {:ok, switch_test} = Breadboard.Switch.connect([pin: :gpio3, direction: :output, initial_value: 0])
       ...>   # pin value is 0
       ...>   0 = Breadboard.Switch.get_value(switch_test)
@@ -58,8 +60,6 @@ defmodule Breadboard.Switch do
       ...> end
       1
 
-  ### turn_on/off an only input pin will terminate the switch
-
   """
 
 
@@ -67,60 +67,59 @@ defmodule Breadboard.Switch do
   Connect to a pin.
 
   ## Options:
-  * :pin - any valid 'pin label' managed by Pinout.label_to_pin
-  * :direction - as defined in the Circuit.GPIO.open
-  * :initial_value - as defined in the Circuit.GPIO.open
+  * `:pin` - any valid 'pin label' managed by `Breadboard.Pinout.label_to_pin/1`
+  * `:direction` - as defined in the Circuit.GPIO.open
+  * `:initial_value` - as defined in the Circuit.GPIO.open
 
   ## Return values
-  On success the function returns `{:ok, pid}`, where `pid` is the PID of the supervised 'switch'
+  On success the function returns `{:ok, switch}`, where `switch` is the PID of the supervised 'Switch'
   """
   def connect(options) do
     Breadboard.DynamicSupervisor.start_switch_server_child(options)
   end
 
   @doc """
-  Set the value 1 of a switch
+  Set the value 1 of a switch (only for `:output` switch)
   """
-  def turn_on(pid) do
-    GenServer.call(pid, :turn_on)
+  def turn_on(switch) do
+    GenServer.call(switch, :turn_on)
   end
 
   @doc """
-  Set the value 0 of a switch
+  Set the value 0 of a switch (only for `:output` switch)
   """
-  def turn_off(pid) do
-    GenServer.call(pid, :turn_off)
+  def turn_off(switch) do
+    GenServer.call(switch, :turn_off)
   end
 
   @doc """
   Read the current value of a switch
   """
-  def get_value(pid) do
-    GenServer.call(pid, :get_value)
+  def get_value(switch) do
+    GenServer.call(switch, :get_value)
   end
 
   @doc """
   Enable or disable pin value change notifications:
 
-  ## Args
-  * `pid` - the switch
-  and key list with:
-  * `trigger` - as defined in Circuits.GPIO.set_interrupts
-  * `opts` - as defined in Circuits.GPIO.set_interrupts
-  * `module` - module where notifications are sent by required specific function signatures defined into `Breadboard.IRQ`
+  * `switch` - the switch
+  * `irq_opts` - keyword list with:
+    - `trigger` - as defined in Circuits.GPIO.set_interrupts
+    - `opts` - as defined in Circuits.GPIO.set_interrupts
+    - `module` - module where notifications are sent by required specific function signatures defined into `Breadboard.IRQ`
 
   ## Return values
-  :ok on success
+  `:ok` on success
   """
-  def set_interrupts(pid, irq_opts) do
-    GenServer.call(pid, {:set_interrupts, irq_opts})
+  def set_interrupts(switch, irq_opts) do
+    GenServer.call(switch, {:set_interrupts, irq_opts})
   end
 
   @doc """
-  Disconnect the switch
+  Disconnect the switch from the pin
   """
-  def disconnect(pid) do
-    Breadboard.DynamicSupervisor.stop_switch_server_child(pid)
+  def disconnect(switch) do
+    Breadboard.DynamicSupervisor.stop_switch_server_child(switch)
   end
 
 
