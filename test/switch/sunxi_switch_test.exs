9ce9a22af42d558f6dd76b639ddaa6b968a37892
henrythebuilder <henrythebuilder@yahoo.it>
2019-10-02 16:28:27 +0200

    Add sunxi integration test

---
+++ test/switch/sunxi_switch_test.exs
@@ -0,0 +1,35 @@
+defmodule SunxiSwitchTest do
+  use ExUnit.Case, async: false
+
+  alias Breadboard.{Switch}
+
+  @tag integration_sunxi: true
+  test "Simple irq test with an external switch and interrupt: PA1 value is reflected to PA20 (a led)" do
+
+    defmodule IRQSwitch do
+      use Breadboard.IRQ
+  #    alias Breadboard.{Switch, IRQ, IRQInfo}
+
+      def interrupt_service_routine(irq_info = %IRQInfo{pin_number: pin_number, timestamp: timestamp, new_value: new_value, pin_label: pin_label}) do
+        # on interrupt turn on/off 'test pin' value
+        IO.puts("irq info='#{inspect(irq_info)}")
+        {:ok, switch_test} = Switch.connect([pin: :pa20, direction: :output])
+        case new_value do
+          1 ->
+            Switch.turn_on(switch_test)
+          0 ->
+            Switch.turn_off(switch_test)
+        end
+        ^new_value = Switch.get_value(switch_test)
+      end
+    end
+
+    {:ok, switch_in} = Switch.connect([pin: :pa1, direction: :input])
+    :ok = Switch.set_interrupts(switch_in, [module: IRQSwitch, trigger: :both, opts: []])
+    IO.puts("Press button on breadboard connected to pin PA1")
+    IO.gets("Return to exit")
+
+  end
+
+
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,15 +1,16 @@
 defmodule TestConfig do
-  def exclude_test() do
+  def exclude_test(excl) do
     case Breadboard.get_platform() do
       :stub ->
-        [:platform_sunxi]
+        [:platform_sunxi|excl]
       :sunxi ->
-        [:platform_stub]
+        [:platform_stub|excl]
       _ ->
         IO.warn("Breadboarding platform undefined !!!")
-        [:platform_stub, :platform_sunxi]
+        [:platform_stub, :platform_sunxi|excl]
     end
   end
 end
 
-ExUnit.start([exclude: TestConfig.exclude_test])
+default_exclude = [:integration_sunxi]
+ExUnit.start([exclude: TestConfig.exclude_test(default_exclude)])
