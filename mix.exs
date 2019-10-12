fbc7346f00182e6b2ffc134e08ec54aa6e3bf13f
henrythebuilder <henrythebuilder@yahoo.it>
2019-10-12 15:48:36 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -2,6 +2,8 @@ defmodule Breadboard.MixProject do
   use Mix.Project
 
   @version "0.1.0"
+  @github_source_url ""
+  @homepage_url @github_source_url
 
   def project do
     [
@@ -9,9 +11,13 @@ defmodule Breadboard.MixProject do
       version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      source_url: @github_source_url,
+      #homepage_url: @homepage_url,
       deps: deps(),
-
-      docs: docs()
+      docs: docs(),
+      aliases: [docs: ["docs", &copy_extra/1]],
     ]
   end
 
@@ -23,11 +29,29 @@ defmodule Breadboard.MixProject do
     ]
   end
 
+  def description() do
+    "An helper library for 'Elixir Circuits'"
+  end
+
+  def package() do
+    %{
+      licenses: ["Apache-2.0"],
+      files: ["lib",
+              "mix.exs",
+              "NOTICE",
+              "LICENSE",
+              "CHANGELOG.md",
+              "README.md",
+              ".formatter.exs"],
+      links: %{"GitHub" => @github_source_url}
+    }
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:circuits_gpio, "~> 0.4"}
-      {:ex_doc, "~> 0.21.0", only: :dev, runtime: false},
+      {:circuits_gpio, "~> 0.4"},
+      {:ex_doc, "~> 0.21.0", only: :docs, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
@@ -36,10 +60,15 @@ defmodule Breadboard.MixProject do
   defp docs() do
     [
       main: "readme",
-      extras: ["README.md",
-               "CHANGELOG.md"
-              ],
+      extras: ["README.md", "CHANGELOG.md"],
       source_ref: "v#{@version}",
+      source_url: @github_source_url
     ]
   end
+
+  defp copy_extra(_) do
+    File.cp("LICENSE", "doc/LICENSE")
+    File.cp("NOTICE", "doc/NOTICE")
+  end
+
 end
